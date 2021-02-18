fb388dee053b1489799428e3f85c6fa3d7266c1c
Muratcan Tüfekçi <1600002065@STU.IKU.EDU.TR>
2021-02-18 20:52:09 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Id = 1;
+            musteri.Ad = "Muratcan";
+            musteri.Soyad = "Tüfekçi";
+            musteri.Eposta = "abcd@hotmail.com";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Uğur";
+            musteri2.Soyad = "Duman";
+            musteri2.Eposta = "abcde@hotmail.com";
+
+            Musteri[] musteriManager2 = new Musteri[] { musteri, musteri2 };
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri);
+            musteriManager.Silme(musteri);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Silme(musteri2);
+            musteriManager.Listeleme(musteriManager2);
+
+            Console.ReadKey();
+        }
+    }
+}
